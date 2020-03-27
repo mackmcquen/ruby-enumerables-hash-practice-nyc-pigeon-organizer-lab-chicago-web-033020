e5a5028789fd19c23c78355f511a5e86fb4b00d6
Mack McQuen <mackmcquen@comcast.net>
2020-03-27 21:45:00 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  result = { }
+  data.each do |attribute, value|
+  value.each do |key, names|
+  names.each do |names|
+  if !result[names]
+    result[names] = {}
+end
+
+  if !result[names][attribute]
+    result[names][attribute] = []
+end
+
+    result[names][attribute].push(key.to_s)
+end
+end
+end
+result
 end
