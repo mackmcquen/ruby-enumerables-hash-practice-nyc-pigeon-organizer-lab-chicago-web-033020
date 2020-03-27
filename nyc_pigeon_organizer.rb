9919b4cbba80c3813dc78f47546acb8ba904f5d5
Mack McQuen <mackmcquen@comcast.net>
2020-03-27 21:44:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,16 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-    result = { }
+  result = { }
   data.each do |attribute, value|
-    value.each do |key, names|
-      names.each do |names|
-        if !result[names]
-          result[names] = {}
-        end
+  value.each do |key, names|
+  names.each do |names|
+  if !result[names]
+    result[names] = {}
+end
 
-        if !result[names][attribute]
-          result[names][attribute] = []
-        end
+  if !result[names][attribute]
+    result[names][attribute] = []
+end
 
         result[names][attribute] << key.to_s
         end
